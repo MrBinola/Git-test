0fcc5585b679706e27e22b0ab7916ed705040876
Code Wizard <codewizard@gmail.com>
2024-11-06 18:35:58 +0200

    VIVI

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 console.log("I love Jesus and Jesus loves me! Because He suffered for me ")
 
 console.log(" I'm MrBinola in YouTube!")
+
+console.log(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+let der = 7
+let fer = 3
+console.log(der + fer)
